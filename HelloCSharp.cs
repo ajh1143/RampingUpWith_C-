e44bf3fc03fa2b76168e6eae2aef4e3bc6b9b0bd
Andrew Holt <ajh.206@outlook.com>
2018-12-06 13:46:41 -0800

    Create HelloCSharp.cs

---
+++ HelloCSharp.cs
@@ -0,0 +1,16 @@
+using System;
+namespace HelloCSharp
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			//Write to console
+			Console.WriteLine("Hello, World");
+			//Read input from console, store as String 'x'
+			String x = Console.ReadLine();
+			//Convert x to uppercase and print to console
+			Console.WriteLine(x.ToUpper());
+		}
+    }
+}
