d8777e896d5774a673a87e2ac336770b6657adae
Andrew Holt <ajh.206@outlook.com>
2018-12-15 14:22:30 -0800

    Update HelloCSharp.cs

--- HelloCSharp.cs
+++ HelloCSharp.cs
@@ -10,7 +10,7 @@ namespace HelloCSharp
 			//Read input from console, store as String 'x'
 			String x = Console.ReadLine();
 			//Convert x to uppercase and print to console
-			Console.WriteLine(x.ToUpper());
+			Console.WriteLine("Hello, {0}", x.ToUpper());
 		}
     }
 }
